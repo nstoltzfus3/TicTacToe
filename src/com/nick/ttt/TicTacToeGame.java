5ee5a1f2805527e3a83b59ba0b30c98e5621be1b
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-11-23 12:14:47 -0500

    Add method gameWon()

--- src/com/nick/ttt/TicTacToeGame.java
+++ src/com/nick/ttt/TicTacToeGame.java
@@ -1,22 +1,44 @@
 package com.nick.ttt;
 
 import javax.security.auth.callback.TextInputCallback;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class TicTacToeGame {
     // TODO: List fields here.
     // Some gameboard representation.
     private char[] gameBoard;
+    private final ArrayList<int[]> winConditions;
+    private final HashMap<Character, Integer> charToInt;
 
     // TODO: Make constructor
     public TicTacToeGame() {
-        this.gameBoard = new char[]{0, 0, 0, 0, 0, 0, 0, 0, 0}; // initialize a blank "board"
+        this.gameBoard = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        this.winConditions = new ArrayList<>();
+        this.winConditions.add(new int[]{0, 1, 2});
+        this.winConditions.add(new int[]{3, 4, 5});
+        this.winConditions.add(new int[]{6, 7, 8});
+        this.winConditions.add(new int[]{0, 3, 6});
+        this.winConditions.add(new int[]{1, 4, 7});
+        this.winConditions.add(new int[]{2, 5, 8});
+        this.winConditions.add(new int[]{0, 4, 8});
+        this.winConditions.add(new int[]{2, 4, 6});
+        this.charToInt = new HashMap<>();
+        charToInt.put('x', -1);
+        charToInt.put('o', 1);
+        charToInt.put(' ', 0);
     }
 
     // TODO: Generate Getters and Setters
     public char[] getGameBoard() {
-        return gameBoard;
+        return this.gameBoard;
+    }
+
+    public ArrayList<int[]> getWinConditions() {
+        return this.winConditions;
+    }
+
+    public HashMap<Character, Integer> getCharToInt() {
+        return charToInt;
     }
 
     // TODO: Required methods below
@@ -47,18 +69,43 @@ public class TicTacToeGame {
     }
 
     public void placePiece(char xo, int pos) {
-        this.getGameBoard()[pos] = xo;
+        if (this.findEmpties().contains(pos)) {
+            this.getGameBoard()[pos] = xo;
+        } else {
+            System.out.println("Not a valid move. Pick another position.");
+        }
     }
 
     public ArrayList<Integer> findEmpties() {
         ArrayList<Integer> empties = new ArrayList<>();
         char[] board = this.getGameBoard();
         for (int i = 0; i < board.length; i++) {
-            if (board[i] == 0) {
+            if (board[i] == ' ') {
                 empties.add(i);
             }
         }
         return empties;
+    }
+
+    public boolean gameWon() {
+        ArrayList<int[]> wins = this.getWinConditions();
+        char[] board = this.getGameBoard();
+        HashMap<Character, Integer> map = this.getCharToInt();
+        int sum = 0;
+
+        for (int[] arr : wins) {
+            for (int i : arr) {
+                if (map.containsKey(board[i])) {
+                    sum += map.get(board[i]);
+                }
+            }
+            if (sum == 3 || sum == -3) {
+                return true;
+            } else {
+                sum = 0;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
