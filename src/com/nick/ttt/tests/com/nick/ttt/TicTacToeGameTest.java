4d6092416d3896df616bf21d3b536d03ec1aac9c
Jared Langson <langson.jared@gmail.com>
2020-11-24 01:01:17 -0500

    Made testing skeleton

---
+++ src/com/nick/ttt/tests/com/nick/ttt/TicTacToeGameTest.java
@@ -0,0 +1,17 @@
+package com.nick.ttt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicTacToeGameTest {
+    TicTacToeGame game = new TicTacToeGame();
+
+    @Test
+    void placePiece() {
+    }
+
+    @Test
+    void findEmpties() {
+    }
+}
