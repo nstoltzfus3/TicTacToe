6245543250e230d9dcf5d4a4a032fbcd89206a47
nstoltzfus <nstoltzfus3@gatech.edu>
2020-11-28 13:45:55 -0500

    Changes to JUnits, and the addition of the Board architecture.

--- src/com/nick/ttt/Player.java
+++ src/com/nick/ttt/Player.java
@@ -8,20 +8,17 @@ public class Player {
     private char symbol; //'X' or 'O' //'X' goes first
 
     //constructor
-    public Player(String name, char symbol, boolean isAI){
-        this.name = name;
-        this.isAI = isAI;
+    public Player(char symbol, boolean isAI){
+
         //if symbol is not 'X' or 'O' throw an error
         if(symbol=='X' || symbol=='O'){
             this.symbol=symbol;
         } else{
             throw new RuntimeException("Symbol must be 'X' or 'O'");
         }
+        this.isAI = isAI;
     }
     //getters
-    public String getName(){
-        return name;
-    }
     public char getSymbol(){
         return symbol;
     }
